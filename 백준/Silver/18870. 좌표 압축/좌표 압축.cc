6dbc6be268447a78b53f033a0e4b534ca3942838
doeeon <doun5824@gmail.com>
2025-06-15 21:31:48 +0900

    [Silver II] Title: 좌표 압축, Time: 1148 ms, Memory: 39724 KB -BaekjoonHub

---
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <map>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	int N;
+	cin >> N;
+
+	int num;
+	vector<int> v;
+	for (int i = 0; i < N; i++)
+	{
+		cin >> num;
+		v.push_back(num);
+	}
+
+	vector<int> sorted_v(v);
+	sort(sorted_v.begin(), sorted_v.end());
+	sorted_v.erase(unique(sorted_v.begin(), sorted_v.end()), sorted_v.end());
+
+	map<int, int> m;
+	for (int i = 0; i < sorted_v.size(); i++)
+		m[sorted_v[i]] = i;
+
+	for (int i = 0; i < N; i++)
+		cout << m[v[i]] << ' ';
+
+	return 0;
+}
