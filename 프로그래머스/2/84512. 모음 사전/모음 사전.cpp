d0b65d185383da1cb7ceb45b7b79c0d999a6bcc7
doeeon <doun5824@gmail.com>
2025-08-09 16:20:11 +0900

    [level 2] Title: 모음 사전, Time: 0.01 ms, Memory: 3.67 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.cpp
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.cpp
@@ -1,33 +1,21 @@
 #include <string>
 #include <vector>
-#include <algorithm>
 #include <iostream>
 using namespace std;
 
 int solution(string word) {
     int answer = 0;
-    
-    string alpha[5] = {"A", "E", "I", "O", "U"};
-    vector<string> dict;
-    dict.push_back("A");
-    dict.push_back("E");
-    dict.push_back("I");
-    dict.push_back("O");
-    dict.push_back("U");
-    
-    int n = 0;
-    for(int i=0; i<4; i++)
+    string ref = "AEIOU";
+    for(int i=0; i<word.size(); i++)
     {
-        int size = dict.size();
-        for(; n<size; n++)
-            for(int k=0; k<5; k++)
-                dict.push_back(dict[n]+alpha[k]);
-
-        n = size;
+        int tmp = ref.find(word[i]);
+        answer += tmp;
+        for(int j=0; j<5-i-1; j++)
+        {
+            tmp *= 5;
+            answer += tmp;
+        }
+        answer ++;
     }
-    
-    sort(dict.begin(), dict.end());
-    answer = find(dict.begin(), dict.end(), word) - dict.begin() + 1;
-    
     return answer;
 }
