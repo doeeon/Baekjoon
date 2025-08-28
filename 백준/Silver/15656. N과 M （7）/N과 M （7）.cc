295b598c358d6c311767637ac6f0317ed1cfec52
doeeon <doun5824@gmail.com>
2025-08-28 20:57:05 +0900

    [Silver III] Title: N과 M (7), Time: 408 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void Backtracking(int depth, int N, int M, vector<int>& num, vector<int>& answer)
+{
+	if (depth == M)
+	{
+		for (int i = 0; i < answer.size(); i++)
+			cout << answer[i] << ' ';
+		cout << '\n';
+		return;
+	}
+	else
+	{
+		for (int i = 0; i < N; i++)
+		{
+			answer.push_back(num[i]);
+			Backtracking(depth + 1, N, M, num, answer);
+			answer.pop_back();
+		}
+	}
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	int N, M;
+	cin >> N >> M;
+
+	vector<int> num(N);
+	int n;
+	for (int i = 0; i < N; i++)
+	{
+		cin >> n;
+		num[i] = n;
+	}
+
+	sort(num.begin(), num.end());
+
+	vector<int> answer;
+	Backtracking(0, N, M, num, answer);
+
+	return 0;
+}
