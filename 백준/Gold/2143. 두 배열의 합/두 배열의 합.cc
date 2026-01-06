d8e724b2f1bdfad8e05070247cd3dc45fead93d5
doeeon <doun5824@gmail.com>
2026-01-06 16:38:14 +0900

    [Gold III] Title: 두 배열의 합, Time: 60 ms, Memory: 8304 KB -BaekjoonHub

--- 백준/Gold/2143. 두 배열의 합/두 배열의 합.cc
+++ 백준/Gold/2143. 두 배열의 합/두 배열의 합.cc
@@ -1,19 +1,19 @@
 #include <iostream>
 #include <vector>
-#include <unordered_map>
+#include <algorithm>
 
 using namespace std;
 
-void MakeSubSum(vector<int>& A, int N, unordered_map<int, int>& m)
+void MakeSubSum(vector<int>& A, int N, vector<int>& sub)
 {
 	for (int i = 0; i < N; i++)
 	{
 		int sum = A[i];
-		m[sum]++;
+		sub.push_back(sum);
 		for (int j = i + 1; j < N; j++)
 		{
 			sum += A[j];
-			m[sum]++;
+			sub.push_back(sum);
 		}
 	}
 }
@@ -36,16 +36,38 @@ int main()
 	for (int i = 0; i < M; i++)
 		cin >> B[i];
 
-	unordered_map<int, int> SubSumA;
-	unordered_map<int, int> SubSumB;
+	vector<int> SubSumA;
+	vector<int> SubSumB;
 
 	MakeSubSum(A, N, SubSumA);
 	MakeSubSum(B, M, SubSumB);
 
+	sort(begin(SubSumA), end(SubSumA));
+	sort(begin(SubSumB), end(SubSumB));
+
 	long long answer = 0;
-	for (auto iter = SubSumA.begin(); iter != SubSumA.end(); iter++)
-		if (SubSumB.find(T - (iter->first)) != SubSumB.end())
-			answer += (long long)iter->second * SubSumB[T - iter->first];
+
+	int left = 0;
+	int right = SubSumB.size()-1;
+	while (left < SubSumA.size() && right >= 0)
+	{
+		int sum = SubSumA[left] + SubSumB[right];
+		if (sum < T)
+			left++;
+		else if (sum > T)
+			right--;
+		else
+		{
+			int tmp_left = left;
+			int tmp_right = right;
+			while (left < SubSumA.size() && SubSumA[left] == SubSumA[tmp_left])
+				left++;
+			while (right >= 0 && SubSumB[right] == SubSumB[tmp_right])
+				right--;
+
+			answer += (long long)(left - tmp_left) * (tmp_right - right);
+		}
+	}
 
 	cout << answer;
 	return 0;
