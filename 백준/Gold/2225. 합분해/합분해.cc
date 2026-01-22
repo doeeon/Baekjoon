40a1f4e2d72fa0265dab55aba00ebb383199f72b
doeeon <doun5824@gmail.com>
2026-01-23 00:00:10 +0900

    [Gold V] Title: 합분해, Time: 0 ms, Memory: 2152 KB -BaekjoonHub

--- 백준/Gold/2225. 합분해/합분해.cc
+++ 백준/Gold/2225. 합분해/합분해.cc
@@ -9,14 +9,16 @@ int main() {
 
 	vector<vector<int>> dp(K + 1, vector<int>(N+1, 0));
 	// dp[i][j] : 정수 i개를 써서 총합 j를 만들 수 있는 경우의 수
-	dp[0][0] = 1;
+	for (int i = 0; i <= K; i++)
+		dp[i][0] = 1;
 
 	for (int i = 1; i <= K; i++)
-		for (int j = 0; j <= N; j++)
+		for (int j = 1; j <= N; j++)
 		{
-			for (int k = 0; k <= j; k++)
-				if (dp[i-1][j - k] > 0)
-					dp[i][j] = (dp[i][j] + dp[i - 1][j - k]) % 1000000000;
+			dp[i][j] = (dp[i][j - 1] + dp[i - 1][j]) % 1000000000;
+			//for (int k = 0; k <= j; k++)
+			//	if (dp[i-1][j - k] > 0)
+			//		dp[i][j] = (dp[i][j] + dp[i - 1][j - k]) % 1000000000;
 		}
 
 	cout << dp[K][N];
