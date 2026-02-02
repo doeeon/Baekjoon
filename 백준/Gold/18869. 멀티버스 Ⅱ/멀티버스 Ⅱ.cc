4c70b3b56131bf8faefa7b27909ea5d67aafa600
doeeon <doun5824@gmail.com>
2026-02-02 19:28:01 +0900

    [Gold V] Title: 멀티버스 Ⅱ, Time: 328 ms, Memory: 49184 KB -BaekjoonHub

--- 백준/Gold/18869. 멀티버스 Ⅱ/멀티버스 Ⅱ.cc
+++ 백준/Gold/18869. 멀티버스 Ⅱ/멀티버스 Ⅱ.cc
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <unordered_map>
-#include <set>
+#include <algorithm>
 
 using namespace std;
 
@@ -14,20 +14,22 @@ int main()
 	cin >> M >> N;
 
 	vector<vector<int>> num(M, vector<int>(N));
-	vector<set<int>> s(M); // 정렬 & 중복값 제거(중복 크기는 같은 순위로)
 	vector<unordered_map<int, int>> m(M); // 각 크기별 순위 기록
 	vector<vector<int>> compressed(M, vector<int>(N)); // 좌표 압축 version
 
 	for (int i = 0; i < M; i++)
 	{
 		for (int j = 0; j < N; j++)
-		{
 			cin >> num[i][j];
-			s[i].insert(num[i][j]);
-		}
 
-		for (auto n : s[i])
-			m[i][n] = m[i].size();
+		vector<int> tmp(num[i]);
+		sort(tmp.begin(), tmp.end());
+
+		for (int j = 0; j < N; j++)
+		{
+			if (j == 0 || tmp[j] != tmp[j - 1])
+				m[i][tmp[j]] = m[i].size();
+		}
 
 		for (int j = 0; j < N; j++)
 			compressed[i][j] = m[i][num[i][j]];
