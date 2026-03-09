cfbe8ad182ae8cdb49f34da57be0de88ae94731d
doeeon <doun5824@gmail.com>
2026-03-09 23:51:32 +0900

    [Silver IV] Title: 큐 2, Time: 316 ms, Memory: 10424 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.cc
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <string>
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	int N;
+	cin >> N;
+
+	queue<int> q;
+
+	string str;
+	int num;
+	for (int i = 0; i < N; i++)
+	{
+		cin >> str;
+
+		if (str == "push")
+		{
+			cin >> num;
+			q.push(num);
+		}
+		else if (str == "pop")
+		{
+			if (q.empty())
+				cout << "-1\n";
+			else
+			{
+				cout << q.front() << '\n';
+				q.pop();
+			}
+		}
+		else if (str == "size")
+			cout << q.size() << '\n';
+		else if (str == "empty")
+			cout << q.empty() << '\n';
+		else if (str == "front")
+		{
+			if (q.empty())
+				cout << "-1\n";
+			else
+				cout << q.front() << '\n';
+		}
+		else if (str == "back")
+		{
+			if (q.empty())
+				cout << "-1\n";
+			else
+				cout << q.back() << '\n';
+		}
+	}
+	return 0;
+}
